84ee553be23ff21311d079fe91b0d05f61ef32d0
hussnain.mohsin <hussnain.mohsin@tekboox.com>
2022-08-01 19:33:36 +0500

    Navbar without design completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from "react";
 //Bootstrap Import In App.js
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
+//Styles Import In App.js
 import "./assets/style/style.scss";
+//Import Header In App.js
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h2>App</h2>
+      <Header />
     </>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
-
-const header = () => {
+import React from "react";
+import { Link } from "react-scroll";
+const Header = () => {
   return (
-    <div>header</div>
-  )
-}
-
-export default header
+    <>
+      <nav className="navbar navbar-expand-lg bg-light">
+        <div className="container-fluid mx-5">
+          <Link className="navbar-brand" to="home">
+            Navbar
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  activeClass="active"
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="about" spy={true} smooth={true}>
+                  About
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="services"
+                  spy={true}
+                  smooth={true}
+                >
+                  Services
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                >
+                  Contact
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+};
+export default Header;
